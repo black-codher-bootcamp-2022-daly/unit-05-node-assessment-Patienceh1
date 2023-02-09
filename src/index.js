648e42fb115012e3e2e806f05d4689b77659f601
Genyus <genyus@gmail.com>
2023-02-09 03:26:05 +0000

    test: implements and activates autograding tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const fs = require('fs');
-const express = require('express');
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 8080;
-const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 const todoFilePath = process.env.BASE_JSON_PATH;
 
 //Read todos from todos.json into variable
@@ -19,12 +19,10 @@ app.use(bodyParser.json());
 app.use("/content", express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
-  
   res.sendFile("./public/index.html", { root: __dirname });
- 
 });
 
-app.get('/todos', (_, res) => {
+app.get("/todos", (_, res) => {
   /*
   res.header("Content-Type","application/json");
   res.sendFile(todoFilePath, { root: __dirname });
@@ -45,9 +43,5 @@ app.get('/todos', (_, res) => {
 //Add POST request with path '/todos/:id/undo
 
 //Add DELETE request with path '/todos/:id
-
-app.listen(port, function () {
-    console.log(`Node server is running... http://localhost:${port}`);
-});
 
 module.exports = app;
